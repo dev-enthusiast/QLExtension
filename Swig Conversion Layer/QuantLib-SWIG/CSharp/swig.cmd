e4944dd1f358b990cac0b2f3386c86af642eae96
letian_zj <letian.zj@gmail.com>
2014-11-22 21:11:36 -0500

    Signed-off-by: letian_zj <letian.zj@gmail.com>

--- Swig Conversion Layer/QuantLib-SWIG/CSharp/swig.cmd
+++ Swig Conversion Layer/QuantLib-SWIG/CSharp/swig.cmd
@@ -1,1 +1,1 @@
-..\..\..\..\swigwin-3.0.2\swig.exe -csharp -c++ -outdir csharp -namespace QuantLib -o cpp\quantlib_wrap.cpp ..\SWIG\quantlib.i
+..\..\..\..\swigwin\swig.exe -csharp -c++ -outdir csharp -namespace QuantLib -o cpp\quantlib_wrap.cpp ..\SWIG\quantlib.i
